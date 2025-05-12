abe330671915cc4735e9d105b8db9a750b813e2a
ShohsultonDev <shohsulton.198@gmail.com>
2025-05-12 14:50:43 +0500

    test

--- src/pages/super-admin/organization-admin/organization-admin.tsx
+++ src/pages/super-admin/organization-admin/organization-admin.tsx
@@ -238,6 +238,7 @@ export const OrganizationAdmin = () => {
             title={t('Ism')}
             placeholder={t('Ismni kiriting')}
             size={'large'}
+            required={true}
           />
           {/* employer_last_name */}
           <InputControl
@@ -246,6 +247,7 @@ export const OrganizationAdmin = () => {
             title={t('Familya')}
             placeholder={t('Familyani kiriting')}
             size={'large'}
+            required={true}
           />
           {/* employer_position */}
           <InputControl
